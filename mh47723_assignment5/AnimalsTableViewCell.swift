7091932130b3322644970b154f42b6c7c0944df8
Ho <mh47723@lib-pclem034.lib.utexas.edu>
2016-09-29 15:23:26 -0500

    Add First Cell Table View Cell

---
+++ mh47723_assignment5/AnimalsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnimalsTableViewCell.swift
+//  mh47723_assignment5
+//
+//  Created by Ho, Minh-Tri on 9/29/16.
+//  Copyright © 2016 Ho, Minh-Tri. All rights reserved.
+//
+
+import UIKit
+
+class AnimalsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
