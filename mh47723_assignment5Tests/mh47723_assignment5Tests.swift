47a2a4eb35a9dc67deb908e0bab1217bfbacce05
iShinta <ho.minhtri.net@gmail.com>
2016-09-30 00:40:37 -0500

    Swift3 Conversion

--- mh47723_assignment5Tests/mh47723_assignment5Tests.swift
+++ mh47723_assignment5Tests/mh47723_assignment5Tests.swift
@@ -28,7 +28,7 @@ class mh47723_assignment5Tests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
